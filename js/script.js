f582bb110b648667851f5887050feef119b3a1cb
Rusik-best86 <119488421+Rusik-best86@users.noreply.github.com>
2023-01-06 13:45:20 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ const headerLinks = document.querySelectorAll('.header__menu')
 headerLinks.forEach((el) => {
    el.addEventListener('click', () => {
       $('.header__burger,.header__menu').toggleClass('active');
+      $('body').toggleClass('lock');
    })
 })
 
